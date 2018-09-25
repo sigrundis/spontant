095fe47f041272ad89a3a86e452d50016816bd08
sigrundish <sigrun@dohop.com>
2018-09-25 21:25:27 +0000

    first commit

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyB-3XUccPxqOrHP1kkO3_Z-_zkXEyT4zBc';
+export const FIREBASE_AUTH_DOMAIN = 'spontant-a73a9.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://spontant-a73a9.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'spontant-a73a9';
+export const FIREBASE_STORAGE_BUCKET = 'spontant-a73a9.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '604583808296';
+
+export const FACEBOOK_APP_ID = '275844459693263';
